5345326fed4a6730a5a73e42d0c442fedd4f80f7
zpri <zoran.pribojevic@sbab.se>
2026-02-11 14:22:25 +0100

    adding vector db

--- src/main/kotlin/com/z/ai_service/AiServiceApplication.kt
+++ src/main/kotlin/com/z/ai_service/AiServiceApplication.kt
@@ -1,11 +1,10 @@
 package com.z.ai_service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication
 @EnableRetry
 class AiServiceApplication
 
